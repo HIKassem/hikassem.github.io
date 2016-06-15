ea5eea6971cd0b893bc0981820ddf86b52af2804
Hassan Kassem <hassan.kassem@gmail.com>
2016-06-15 12:24:43 +0100

    Nikola auto commit.
    Nikola version: 7.7.9

--- conf.py
+++ conf.py
@@ -819,7 +819,7 @@ LICENSE = """
 <img alt="Creative Commons License" style="border-width:0"
 src="https://i.creativecommons.org/l/by-nc-sa/4.0/80x15.png" /></a><br />
 This work by <a xmlns:cc="http://creativecommons.org/ns#"
-href="HassanKassem.me" property="cc:attributionName"
+href="#" property="cc:attributionName"
 rel="cc:attributionURL">Hassan Kassem</a> is licensed under a
 <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/">
 Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International
